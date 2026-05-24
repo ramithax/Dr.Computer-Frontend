4db34c445c67524a42196bdeda14f6829a8b40a3
ramitha <ramithamanilka2003@gmail.com>
2026-05-24 18:31:40 +0530

    Display products

--- src/pages/Admin/adminproductPage.jsx
+++ src/pages/Admin/adminproductPage.jsx
@@ -1,6 +1,10 @@
 import FloatingAddButton from "../../components/addbutton"
+import { useState } from "react"
 
 export default function AdminProductsPage(){
+
+    const [products,setProducts]=useState([])
+
     return(
         <div className="w-full h-full relative">
             <h1 className="text-3xl font-bold text-gray-500">Admin Products Page</h1>
