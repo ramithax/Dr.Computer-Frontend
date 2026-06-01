a284875cd1b8878deed5de02b83f1197a5f9efa2
ramitha <ramithamanilka2003@gmail.com>
2026-06-01 20:37:38 +0530

    product table

--- src/components/deleteButton.jsx
+++ src/components/deleteButton.jsx
@@ -1,1 +1,52 @@
-export default function DeleteButton({productId,onDelete}){}
+import { useState } from "react";
+import { CiTrash } from "react-icons/ci";
+import { IoClose } from "react-icons/io5";
+import api from "../utils/api";
+import toast from "react-hot-toast";
+
+export default function DeleteButton(props){
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const refresh = props.refresh;
+    const productId = props.productId;
+
+    return (
+        <>
+            <CiTrash className="text-red-600 text-xl rounded-full hover:border cursor-pointer " onClick={() => setIsModalVisible(true)} />
+            {
+                isModalVisible && (
+                    <div className="w-screen h-screen bg-black/70 fixed left-0 top-0 z-50 flex justify-center items-center">
+
+                        <div className="w-[400px] h-[200px] bg-white rounded-lg flex flex-col overflow-hidden justify-between">
+                            <div className="w-full h-[40px] bg-accent flex justify-between items-center px-4">
+                                <h1 className="text-white text-lg font-semibold ">Confirm Deletion</h1>
+                                <IoClose className="text-white hover:bg-red-600 cursor-pointer" onClick={
+                                    () => setIsModalVisible(false)
+                                }/>
+                            </div>
+                            <p className="p-4 text-center text-gray-700">Are you sure you want to delete this product with ID {productId}?</p>
+                            <div className="w-full flex p-2 justify-center items-center gap-4">
+                                
+                                <button className="w-[100px] p-2 text-white bg-gray-700 rounded-sm hover:bg-gray-800" onClick={() => setIsModalVisible(false)}>Cancel</button>
+                                <button className=" w-[100px] p-2 text-white bg-red-700 rounded-sm hover:bg-red-800" onClick={() => {
+                                    const token = localStorage.getItem("token");
+                                    api.delete("/products/" + productId, {
+                                        headers: {
+                                            Authorization: `Bearer ${token}`,
+                                        },
+                                    }).then(() => {
+                                        refresh();
+                                        setIsModalVisible(false);
+                                    }).catch(() => {
+                                        toast.error("Error deleting product");
+                                    });
+                                }}>Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </>
+    )
+}
--- src/pages/Admin/adminproductPage.jsx
+++ src/pages/Admin/adminproductPage.jsx
@@ -1,90 +1,98 @@
-import FloatingAddButton from "../../components/addbutton"
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import api from "../../utils/api";
-import toast, { Toaster } from "react-hot-toast";
 import LoadingScreen from "../../components/loadingScreen";
+import DeleteButton from "../../components/DeleteButton";
 
-export default function AdminProductsPage(){
 
-    const [products,setProducts]=useState([])
-    const [loading,setLoading]=useState(true)
+export default function AdminProductsPage() {
+	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
-        if(loading){
-            const token=localStorage.getItem("token")
-        api.get("/products",{
-            headers:{
-                Authorization : `Bearer ${token}`
-            }
-        }).then((res)=>{
-            console.log(res.data)
-            setProducts(res.data)
-            setLoading(false)
-        })
-        }
-    }
-    ,[loading]);
+	useEffect(() => {
+		if (loading) {
+			const token = localStorage.getItem("token");
+			api
+				.get("/products", {
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				})
+				.then((res) => {
+					console.log(res.data);
+					setProducts(res.data);
+					setLoading(false);
+				});
+		}
+	}, [loading]);
 
-    return(
-        <div className="w-full h-full relative">
-            {
-                loading && <LoadingScreen />
-            }
-            <table>
-                <thead>
-                    <tr>
-                        <th>-</th>
-                        <th>ProductID</th>
-                        <th>Name</th>
-                        <th>price</th>
-                        <th>Labeled price</th>
-                        <th>Brand</th>
-                        <th>Model</th>
-                        <th>Category</th>
-                        <th>Availability</th>
-                        <th>Stock</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        products.map((product)=>{
-                            return <tr key={product.productId}>
-                                <td>
-                                    <img src={product.images[0]} alt={product.name} className="w-16 h-16 rounded"/>
-                                </td>
-                                <td>{product.productId}</td>
-                                <td>{product.name}</td>
-                                <td>{product.price}</td>
-                                <td>{product.labelledPrice}</td>
-                                <td>{product.brand}</td>
-                                <td>{product.model}</td>
-                                <td>{product.category}</td>
-                                <td>{product.isAvailable ? "Available" : "Out of stock"}</td>
-                                <td>{product.stock}</td>
-                                <td>
-                                    <button className="w-[100px] bg-red-500 text-white hover:bg-red-700 rounded" onClick={
-                                        ()=>{
-                                            const token=localStorage.getItem("token")
-                                            api.delete("/products/"+product.productId,{
-                                                headers:{
-                                                    Authorization:`Bearer ${token}`
-                                                }
-                                            }).then(()=>{
-                                                toast.success("Product deleted successfully")
-                                                setLoading(!loading)
-                                            }).catch(()=>{
-                                                toast.error("Error deleting product")
-                                            })
-                                        }
-                                    }>Delete</button>
-                                </td>
-                            </tr>
-                        })
-                    }
-                </tbody>
-            </table>
-            <FloatingAddButton />
-        </div>
-    )
+	//backend call products fetch and setProducts
+
+	return (
+		<div className="w-full h-full ">
+			<div className="w-full h-[100px] bg-white shadow-2xl mb-10 rounded-lg flex p-4 items-center justify-between">
+                <h1 className="text-2xl font-semibold">All Products</h1>
+                <div className="h-full gap-4 flex items-center">
+                    {products.length} Products
+                </div>                
+            </div>
+			{
+				loading && <LoadingScreen/>
+			}
+			<table className="w-full text-center rounded-lg overflow-hidden">
+				<thead className="bg-accent text-white h-[40px] ">
+					<tr>
+						<th className="w-[5%]"></th>
+						<th className="w-[7%]">Product ID</th>
+						<th className="w-[22%]">Name</th>
+						<th className="w-[9%]">Price</th>
+						<th className="w-[9%]">Labelled Price</th>
+						<th className="w-[7%]">Brand</th>
+						<th className="w-[7%]">Model</th>
+						<th className="w-[9%]">Category</th>
+						<th className="w-[5%]">Availability</th>
+						<th className="w-[5%]">Stock</th>
+						<th className="w-[15%]">Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					{products.map((product) => {
+						return (
+							<tr className="odd:bg-gray-300 even:bg-white h-[60px]" key={product.productId}>
+								<td>
+									<img
+										src={product.images[0]}
+										alt={product.name}
+										className="w-16 h-16  rounded"
+									/>
+								</td>
+								<td>{product.productId}</td>
+								<td>{product.name}</td>
+								<td>{product.price}</td>
+								<td>{product.labelledPrice}</td>
+								<td>{product.brand}</td>
+								<td>{product.model}</td>
+								<td>{product.category}</td>
+								<td>{product.isAvailable ? "Available" : "Out of Stock"}</td>
+								<td>{product.stock}</td>
+								<td>
+									<div className="w-full flex justify-center items-center gap-4">
+										<DeleteButton productId={product.productId} refresh={()=>setLoading(true)} />
+									</div>
+								</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+
+			<Link
+				to="/admin/add-product"
+				className="bg-accent w-[80px] h-[80px] rounded-full text-white text-2xl flex justify-center items-center fixed bottom-4 right-4 shadow-2xl hover:bg-white hover:text-accent"
+			>
+				<FaPlus />
+			</Link>
+		</div>
+	);
 }
